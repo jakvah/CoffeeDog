730997ec44ca5a458695b4ecb116e719213a64ee
MatDog Haugum <matias_haugum@hotmail.com>
2021-09-27 21:36:28 +0200

    ready for release

--- hardware/hardware.py
+++ hardware/hardware.py
@@ -1,6 +1,7 @@
 import rdm6300
 import time
 import datetime
+import os
 from gpiozero import TonalBuzzer, LED
 from gpiozero.tones import Tone
 
@@ -13,8 +14,8 @@ queue = {}
 
 # ---------- SETUP ----------#
 
-VERSION = 0.3
-COFFEE_TIMEOUT = 5 # Time to wait until you can dog another coffee
+VERSION = 1.0
+COFFEE_TIMEOUT = 2*60 # Time to wait until you can dog another coffee
 
 
 # ---------------------------#
@@ -24,14 +25,28 @@ def send_coffee_data(id,timestamp):
     url = f"https://jakvah.pythonanywhere.com/add_coffee/{id}/{timestamp}"
     #data_obj = {"id":id,"timestamp":timestamp}
     r = requests.post(url)
-    now = datetime.datetime.now()
-    print (str(now.hour) + ":" + str(now.minute) + ":" + str(now.second) + " - " + r.text)
+    if r.text == "200":
+        return True
+    elif r.text == "300":
+        now = datetime.datetime.now()
+        print (str(now.hour) + ":" + str(now.minute) + ":" + str(now.second) + " - Card not registered! Please register!")
+        return False
+    else:
+        return False
 
-def play_tune(cardvalue):
-    #customTone = (str(cardvalue)[-4:])
-    #for number in customTone:
-    #    b.play(Tone(int(220+int(number)*66)))
-    #    time.sleep(0.1)
+def play_tune():
+
+    b.play(Tone(int(220+66*1)))
+    time.sleep(0.05)
+    b.play(Tone(int(220+66*2)))
+    time.sleep(0.05)
+    b.play(Tone(int(220+66*3)))
+    time.sleep(0.05)
+    b.play(Tone(int(220+66*4)))
+    time.sleep(0.05)
+    b.stop()
+
+def play_succeeded():
     b.play(Tone(int(220+66*2)))
     time.sleep(0.05)
     b.play(Tone(int(220+66*4)))
@@ -66,7 +81,6 @@ def check_dict(card_id):
     if card_id in queue: #Recently registered
         currenttime = int(time.time())
         if (currenttime - int(queue[card_id])) < COFFEE_TIMEOUT:
-            print("")
             print("Litegranne snøgg, please wait " + str(COFFEE_TIMEOUT - (currenttime - int(queue[card_id]))) + " seconds.")
             return False
         else: #Refresh time
@@ -88,16 +102,23 @@ def read_card():
                 if (check_dict(str(card.value))):
                     blue_led.off()
                     blue_led.blink(0.075,0.075,30)
-                    play_tune(card.value)
-                    send_coffee_data(card.value,get_timestamp())
-                    print("---------------")
+                    play_tune()
+                    if send_coffee_data(card.value,get_timestamp()):
+                        now = datetime.datetime.now()
+                        print (str(now.hour) + ":" + str(now.minute) + ":" + str(now.second) + " - Card registered and score updated.")
+                        play_succeeded()
+                    else:
+                        blue_led.off()
+                        red_led.on()
+                        play_declined()
+                        time.sleep(1)
+                        red_led.off()
                 else:
                     blue_led.off()
                     red_led.on()
                     play_declined()
                     time.sleep(1)
                     red_led.off()
-                    print("---------------")
 
 # clean_dict() - periodically cleans out old cards from the dictionary. 
 def clean_dict():
@@ -112,18 +133,15 @@ def clean_dict():
 
 
 
-
+os.system('cls' if os.name == 'nt' else 'clear')
 print ("---------------------")
 print ("   COFFEEDOG V." + str(VERSION) )
 print ("---------------------")
 
 print("       ▄      ▄  \n      ▐▒▀▄▄▄▄▀▒▌   \n    ▄▀▒▒▒▒▒▒▒▒▓▀▄  sniff\n  ▄▀░█░░░░█░░▒▒▒▐   sniff - hunden Sverre\n  ▌░░░░░░░░░░░▒▒▐  \n ▐▒░██▒▒░░░░░░░▒▐  \n ▐▒░▓▓▒▒▒░░░░░░▄▀  \n  ▀▄░▀▀▀▀░░░░▄▀     \n    ▀▀▄▄▄▄▄▀▀       ")
 print("----------------------")
+
 while True:
     blue_led.on()
     read_card()
     clean_dict()
-
-
-
-
